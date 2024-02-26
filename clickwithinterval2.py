8b87c9ae3b40859d0a2fb7333ff9f71bb73cd9c8
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-26 16:50:16 +0530

    Update clickwithinterval2.py

--- clickwithinterval2.py
+++ clickwithinterval2.py
@@ -25,8 +25,14 @@ def run_with_confirmation(command):
 
 def install_packages():
     subprocess.run(["apt", "update"])
-    subprocess.run(["pip", "install", "requests", "pyautogui"])
     run_with_confirmation(['sudo', 'apt', 'install', '-y', 'python3-pip'])
+    subprocess.run(['pip3', 'install', '--upgrade', 'pip'])
+    subprocess.run(['pip3', 'install', 'pynput'])
+    subprocess.run(['pip3', 'install', 'pyautogui'])
+
+
+
+
 
 def download_file(url, filename):
     response = requests.get(url)
