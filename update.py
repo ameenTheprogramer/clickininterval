bc367444433a799b3d976cf9f242582a55fe38ba
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-26 18:01:23 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import subprocess
+
+def check_and_delete_files():
+    current_dir = os.getcwd()
+    clickwithinterval4_path = os.path.join(current_dir, 'clickwithinterval4.py')
+    second_py_path = os.path.join(current_dir, '2nd.py')
+
+    if os.path.exists(clickwithinterval4_path):
+        os.remove(clickwithinterval4_path)
+        print("Deleted clickwithinterval4.py")
+
+    if os.path.exists(second_py_path):
+        os.remove(second_py_path)
+        print("Deleted 2nd.py")
+
+def download_file(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+            print(f"Downloaded {filename}")
+    else:
+        print(f"Failed to download {filename}")
+
+def main():
+    # Check and delete existing files
+    check_and_delete_files()
+
+    # Download clickwithinterval4.py
+    url = "https://github.com/ameenTheprogramer/clickininterval/raw/main/clickwithinterval4.py"
+    filename = "clickwithinterval4.py"
+    download_file(url, filename)
+
+if __name__ == "__main__":
+    main()
+    subprocess.run(['python', 'clickwithinterval4.py'])
